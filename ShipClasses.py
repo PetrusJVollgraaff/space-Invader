d29238b0eeaab73657d332365dfc27917be0f5f3
PetrusJVollgraaff <petrusvollgraaff1988@hotmail.co.za>
2024-02-27 16:43:30 +0200

    Update

--- ShipClasses.py
+++ ShipClasses.py
@@ -2,20 +2,22 @@ import pygame
 
 class ShipBullet:
 
-    def __init__(self, x_axis, y_axis):
+    def __init__(self, x_axis, y_axis, color):
         self.x = x_axis
         self.y = y_axis
         self.didHit = False
+        self.color = color
 
     def drawbullet(self, win):
-        return pygame.draw.line(win, (128, 128, 128), (self.x, self.y),
+        return pygame.draw.line(win, self.color, (self.x, self.y),
                          (self.x, self.y + 5))
 
 class Ship:
-    def __init__(self, x_axis, y_axis, ):
+    def __init__(self, x_axis, y_axis):
         self.x = x_axis
         self.y = y_axis
         self.isHit = False
+        self.player = "enemy"
         self.bullet_list = []
         self.color = (255, 255, 255)
 
@@ -36,7 +38,12 @@ class Ship:
             self.x += 1
 
     def shoot(self, win):
-        bullet = ShipBullet(self.x, self.y)
+        bulletcolor = (125,125,125)
+
+        if self.player == "player":
+            bulletcolor = (0, 255, 0)
+
+        bullet = ShipBullet(self.x, self.y, bulletcolor)
         self.bullet_list.append( bullet )
         bullet.drawbullet(win)
 
@@ -57,6 +64,7 @@ class SpaceShip(Ship):
     def __init__(self, x_axis, y_axis):
         super().__init__(x_axis, y_axis)
         self.color = (255, 255, 255)
+        self.player = "player"
 
 
 class EnemyShip(Ship):
@@ -64,3 +72,4 @@ class EnemyShip(Ship):
     def __init__(self, x_axis, y_axis):
         super().__init__(x_axis, y_axis)
         self.color = (255, 0, 0)
+        self.player = "enemy"
